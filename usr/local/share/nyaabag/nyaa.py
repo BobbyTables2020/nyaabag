784e1b7202421f40b4e45ecfb7b45818562597a7
BobbyTables2020 <mburke20@seas.upenn.edu>
2022-04-11 19:53:38 -0400

    Update nyaa.py

--- usr/local/share/nyaabag/nyaa.py
+++ usr/local/share/nyaabag/nyaa.py
@@ -1,6 +1,8 @@
 import requests
 import utils
 import torrent
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 
 class Nyaa:
@@ -8,9 +10,14 @@ class Nyaa:
     def __init__(self):
         self.SITE = utils.TorrentSite.NYAASI
         self.URL = "https://nyaa.si"
+        self.session = requests.Session()
+        retry = Retry(connect=99, backoff_factor=0.5)
+        adapter = HTTPAdapter(max_retries=retry)
+        self.session.mount('http://', adapter)
+        self.session.mount('https://', adapter)
 
     def last_uploads(self, number_of_results):
-        r = requests.get(self.URL)
+        r = self.session.get(self.URL)
 
         # If anything up with nyaa servers let the user know.
         r.raise_for_status()
@@ -37,11 +44,11 @@ class Nyaa:
             user_uri = ""
 
         if page > 0:
-            r = requests.get("{}/{}?f={}&c={}_{}&q={}&p={}".format(
+            r = self.session.get("{}/{}?f={}&c={}_{}&q={}&p={}".format(
                 url, user_uri, filters, category, subcategory, keyword,
                 page))
         else:
-            r = requests.get("{}/{}?f={}&c={}_{}&q={}".format(
+            r = self.session.get("{}/{}?f={}&c={}_{}&q={}".format(
                 url, user_uri, filters, category, subcategory, keyword))
 
         r.raise_for_status()
@@ -55,7 +62,7 @@ class Nyaa:
         return torrent.json_to_class(json_data)
 
     def get(self, view_id):
-        r = requests.get(f'{self.URL}/view/{view_id}')
+        r = self.session.get(f'{self.URL}/view/{view_id}')
         r.raise_for_status()
 
         json_data = utils.parse_single(request_text=r.text, site=self.SITE)
@@ -63,7 +70,7 @@ class Nyaa:
         return torrent.json_to_class(json_data)
 
     def get_user(self, username):
-        r = requests.get(f'{self.URL}/user/{username}')
+        r = self.session.get(f'{self.URL}/user/{username}')
         r.raise_for_status()
 
         json_data = utils.parse_nyaa(
